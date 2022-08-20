af08eebe9af961354ae94122589b07ec1107b9b3
Andreas Ekeroth <andreas@plingot.com>
2022-08-20 22:11:48 +0200

    started with subscription extension

---
+++ apps/Service/Tibber/Extensions/PriceRatingQueryBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tibber.Sdk;
+
+namespace NetDaemonApps.apps.Service.Tibber.Extensions
+{
+    public class PriceRatingQueryBuilder : GraphQlQueryBuilder<PriceRatingQueryBuilder>
+    {
+        private static readonly FieldMetadata[] AllFieldMetadata =
+        {
+            new FieldMetadata { Name = "thresholdPercentages", IsComplex = true, QueryBuilderType = typeof(PriceQueryBuilder) },
+            new FieldMetadata { Name = "hourly", IsComplex = true, QueryBuilderType = typeof(PriceQueryBuilder) },
+            new FieldMetadata { Name = "daily", IsComplex = true, QueryBuilderType = typeof(PriceQueryBuilder) },
+            new FieldMetadata { Name = "monthly", IsComplex = true, QueryBuilderType = typeof(SubscriptionPriceConnectionQueryBuilder) }
+        };
+        protected override string TypeName { get; } = "PriceRating";
+
+        protected override IList<FieldMetadata> AllFields { get; } = AllFieldMetadata;
+        public PriceRatingQueryBuilder(string alias = null, SkipDirective skip = null, IncludeDirective include = null) : base(alias, new GraphQlDirective[] { skip, include })
+        {
+        }
+    }
+}
---
+++ apps/Service/Tibber/Extensions/SubscriptionQueryBuilderExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tibber.Sdk;
+
+namespace NetDaemonApps.apps.Service.Tibber.Extensions
+{
+    internal class SubscriptionQueryBuilderExtension : SubscriptionQueryBuilder
+    {
+        private static readonly FieldMetadata[] AllFieldMetadata =
+        {
+            new FieldMetadata { Name = "id" },
+            new FieldMetadata { Name = "subscriber", IsComplex = true, QueryBuilderType = typeof(LegalEntityQueryBuilder) },
+            new FieldMetadata { Name = "validFrom" },
+            new FieldMetadata { Name = "validTo" },
+            new FieldMetadata { Name = "status" },
+            new FieldMetadata { Name = "priceInfo", IsComplex = true, QueryBuilderType = typeof(PriceInfoQueryBuilder) }
+        };
+        protected override string TypeName { get; } = "Subscription";
+        protected override IList<FieldMetadata> AllFields { get; } = AllFieldMetadata;
+        public SubscriptionQueryBuilderExtension(string alias = null, SkipDirective skip = null, IncludeDirective include = null) : base(alias, skip, include)
+        {
+        }
+
+        public SubscriptionQueryBuilder WithPriceRating(PriceRatingQueryBuilder priceRatingQueryBuilder) => WithObjectField("priceRating", priceRatingQueryBuilder);
+    }
+}
