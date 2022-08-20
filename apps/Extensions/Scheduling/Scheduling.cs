4307a5a25c3c62a000154aaacdbadb9cf0ab334e
Andreas Ekeroth <andreas@plingot.com>
2022-08-20 18:14:33 +0200

    Changed time to Datetime.now to depend on timezone on host

--- apps/Extensions/Scheduling/Scheduling.cs
+++ apps/Extensions/Scheduling/Scheduling.cs
@@ -15,22 +15,14 @@ public class SchedulingApp
     public SchedulingApp(IHaContext ha, INetDaemonScheduler scheduler,TibberService tibberService, TibberAutomation tibberAutomation)
     {
         var count = 0;
-        var now = DateTime.UtcNow.AddHours(2);
+        var now = DateTime.Now;
         var starttime = DateTime.Today.AddHours(now.Hour+1).AddMinutes(3);
         scheduler.RunEvery(TimeSpan.FromHours(1), new DateTimeOffset(starttime), () => {
             
             var price = tibberService.GetCurrentPriceAsync().Result;
             tibberAutomation.CurrentPriceAutomations(price);
             ha.CallService("notify", "persistent_notification",
-                data: new { message = $"This is a scheduled action! run at {now} and current price is {price?.Total}{price?.Currency}", title = "Schedule!" });
+                data: new { message = $"This is a scheduled action! run at {DateTime.Now} and current price is {price?.Total}{price?.Currency}", title = "Schedule!" });
         });
-        //scheduler.RunEvery(TimeSpan.FromHours(1), () =>
-        //{
-            
-        //    // Make sure we do not flood the notifications :)
-        //    if (count++ < 3)
-        //        ha.CallService("notify", "persistent_notification",
-        //            data: new { message = "This is a scheduled action!", title = "Schedule!" });
-        //});
     }
 }
